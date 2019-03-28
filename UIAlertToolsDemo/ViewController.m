872c4753187e665a814724a4e306a81db6bdd75c
HLoveff <hzs120203@126.com>
2019-03-28 11:42:21 +0800

    project init

--- UIAlertToolsDemo/ViewController.m
+++ UIAlertToolsDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "HLAlertTool/HLAlertTool.h"
 
 @interface ViewController ()
 
@@ -18,6 +19,37 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)alert:(id)sender {
+    HLAlertTool *alertTool = [[HLAlertTool alloc] init];
+    [alertTool showAlertViewWithAlertTitle:@"提示" alertMessage:@"是否确定要取消" cancelTitle:@"取消" comfirTitle:@"确定" comfirHandle:^{
+        NSLog(@"确定");
+    } cancelHandle:^{
+        NSLog(@"取消");
+    }];
+}
 
+- (IBAction)sheet:(id)sender {
+    HLAlertTool *sheetTool = [[HLAlertTool alloc] init];
+    [sheetTool showSheetViewWithSheetTitle:@"提示" sheetMessage:@"" cancelTitle:@"取消" comfirTitle:@"确定" comfirHandle:^{
+        NSLog(@"确定");
+    } cancelHandle:^{
+        NSLog(@"取消");
+    }];
+}
+
+- (IBAction)more:(id)sender {
+    HLAlertTool *alertTool = [[HLAlertTool alloc] initWithTitle:@"提示" message:@"删除将不再出现"];
+    [alertTool addAlertActonWithTitle:@"取消" alertType:HLAlertTypeCancel aciton:^{
+        
+    }];
+    [alertTool addAlertActonWithTitle:@"确定" alertType:HLAlertTypeDefault aciton:^{
+        
+    }];
+    
+    [alertTool addAlertActonWithTitle:@"再想想" alertType:HLAlertTypeDestructive aciton:^{
+        
+    }];
+    [alertTool showAlert];
+}
 
 @end
