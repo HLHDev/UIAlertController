39b81668ffdcb85b07ffcdabf7840eaf6a020ccc
huangzhangsheng <903521766@qq.com.com>
2019-03-28 09:29:18 +0800

    Initial Commit

---
+++ UIAlertToolsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIAlertToolsDemo
+//
+//  Created by H&L on 2019/3/28.
+//  Copyright © 2019 hzsmac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
